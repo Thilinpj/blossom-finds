37c9d43007367b7ece3428e53ba1f0c60b410bb3
Thilin <thilini_pramodha@yahoo.com>
2025-06-02 13:28:51 +0530

    💗 Styled navbar pastel pink and centered category links

--- BlossomFinds.Api/obj/Debug/net8.0/BlossomFinds.Api.AssemblyInfo.cs
+++ BlossomFinds.Api/obj/Debug/net8.0/BlossomFinds.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlossomFinds.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ef6b45ced8cbfb5c0359613ed4d59b9add0b58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4514b22d88c6915c6d4056152d9496e0eeb579b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlossomFinds.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlossomFinds.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
